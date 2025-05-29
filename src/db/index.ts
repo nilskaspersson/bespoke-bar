d93374e7118e6a79f2be4122e5acd4306e11dfb3
Nils Kaspersson <nils.kaspersson@live.se>
2025-05-29 09:05:42 +0200

    fix: Use actual environmental indicator to determine connection type

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,9 +7,9 @@ if (!process.env.DATABASE_URL) {
 	throw new Error("DATABASE_URL is not set");
 }
 
-const isProduction = process.env.NODE_ENV === "production";
+const onVercel = process.env.VERCEL === "1";
 
-const db = isProduction
+const db = onVercel
 	? drizzleNeon(neon(process.env.DATABASE_URL))
 	: drizzle(process.env.DATABASE_URL);
 
