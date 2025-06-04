885268bb001f85f80812b13f2387134b2ec54ffa
Nils Kaspersson <nils.kaspersson@live.se>
2025-06-04 16:52:10 +0200

    fix: Skip function invokation, overscroll menu

--- src/app/components/BarNavigation/index.tsx
+++ src/app/components/BarNavigation/index.tsx
@@ -13,7 +13,6 @@ export function BarNavigation({
 	...props
 }: Omit<HTMLAttributes<HTMLDivElement>, "children">) {
 	const pathname = usePathname();
-	const getIsActive = (path: string) => pathname === path;
 
 	return (
 		<nav className={clsx(styles.nav, className)} {...props}>
@@ -23,44 +22,44 @@ export function BarNavigation({
 				</div>
 
 				<Text as="ul" size={2} compact className={styles.list}>
-					<li>
+					<li className={styles.item}>
 						<Link
 							href="/bar"
 							className={clsx(styles.link, {
-								[styles.isActive]: getIsActive("/bar"),
+								[styles.isActive]: pathname === "/bar",
 							})}
 						>
 							Overview
 						</Link>
 					</li>
 
-					<li>
+					<li className={styles.item}>
 						<Link
 							href="/bar/recipes"
 							className={clsx(styles.link, {
-								[styles.isActive]: getIsActive("/bar/recipes"),
+								[styles.isActive]: pathname === "/bar/recipes",
 							})}
 						>
 							Recipes
 						</Link>
 					</li>
 
-					<li>
+					<li className={styles.item}>
 						<Link
 							href="/bar/lists"
 							className={clsx(styles.link, {
-								[styles.isActive]: getIsActive("/bar/lists"),
+								[styles.isActive]: pathname === "/bar/lists",
 							})}
 						>
 							Lists
 						</Link>
 					</li>
 
-					<li>
+					<li className={styles.item}>
 						<Link
 							href="/bar/ingredients"
 							className={clsx(styles.link, {
-								[styles.isActive]: getIsActive("/bar/ingredients"),
+								[styles.isActive]: pathname === "/bar/ingredients",
 							})}
 						>
 							Ingredients
