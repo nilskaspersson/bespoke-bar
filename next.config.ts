948b20370a2e897d8cea154161ef7cfc7eb3beaf
Nils Kaspersson <nils.kaspersson@live.se>
2025-06-04 20:32:46 +0200

    hack(layout): Ensure CSS At-layers are defined in the correct order

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+	experimental: {
+		inlineCss: true,
+		useLightningcss: true,
+	},
+};
 
 export default nextConfig;
