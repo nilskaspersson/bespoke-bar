99b54d7712ff2e43856ce51cad228ba52291e551
Nils Kaspersson <nils.kaspersson@live.se>
2025-06-12 19:28:50 +0200

    feat(ui): Add helperText prop, improve a11y

--- src/ui/ControlLabel/index.tsx
+++ src/ui/ControlLabel/index.tsx
@@ -4,19 +4,21 @@ import styles from "./styles.module.css";
 
 export function ControlLabel(props: {
 	className?: string;
-	id?: string;
 	label: React.ReactNode;
 	children: React.ReactNode;
 	required?: boolean;
+	htmlFor: string;
+	id: string;
 }) {
 	return (
 		<div className={clsx(styles.base, props.className)}>
 			<Text
+				id={props.id}
 				as="label"
 				size={2}
 				weight={500}
 				compact
-				htmlFor={props.id}
+				htmlFor={props.htmlFor}
 				className={clsx(styles.label, {
 					required: props.required,
 				})}
