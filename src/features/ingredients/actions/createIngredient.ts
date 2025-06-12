a5b2ac774f8cfcbbec5f411ce31a00d5e91848a5
Nils Kaspersson <nils.kaspersson@live.se>
2025-06-12 19:31:03 +0200

    feat(ingredients): Create + Delete actions

---
+++ src/features/ingredients/actions/createIngredient.ts
@@ -0,0 +1,35 @@
+import { db } from "@/db";
+import {
+	type DraftIngredient,
+	type Ingredient,
+	IngredientsTable,
+	insertIngredientSchema,
+} from "@/db/schema/ingredients";
+import { authOrForbidden } from "@/utils/auth";
+
+/**
+ * Create a new ingredient in the database.
+ * @param userInputIngredient - The ingredient data from user input
+ * @returns Newly created ingredient
+ */
+export async function createIngredient(
+	userIngredient: DraftIngredient,
+): Promise<Ingredient> {
+	"use server";
+
+	const { userId, orgId } = await authOrForbidden();
+
+	const validatedUserInputIngredient = insertIngredientSchema.parse({
+		...userIngredient,
+		orgId,
+		createdBy: userId,
+		updatedBy: userId,
+	});
+
+	const [ingredient] = await db
+		.insert(IngredientsTable)
+		.values(validatedUserInputIngredient)
+		.returning();
+
+	return ingredient;
+}
---
+++ src/features/ingredients/actions/deleteIngredient.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { and, eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+import { type Ingredient, IngredientsTable } from "@/db/schema/ingredients";
+import { revalidateIngredientPaths } from "@/features/ingredients/utils";
+import { authOrForbidden } from "@/utils/auth";
+
+export async function deleteIngredient({
+	id,
+	redirectTo,
+}: {
+	id: Ingredient["id"];
+	redirectTo?: string;
+}): Promise<void> {
+	const { orgId } = await authOrForbidden();
+
+	await db
+		.delete(IngredientsTable)
+		.where(and(eq(IngredientsTable.id, id), eq(IngredientsTable.orgId, orgId)));
+
+	revalidateIngredientPaths(id);
+
+	if (redirectTo) {
+		redirect(redirectTo);
+	}
+}
