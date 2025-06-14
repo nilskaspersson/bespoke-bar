7256f5c984676c094b3d02174687824d01611d60
Nils Kaspersson <nils.kaspersson@live.se>
2025-06-14 23:26:20 +0200

    test(specs): Update test cases to new API

--- src/features/specs/utils/userInputToSpec/userInputToSpec.test.ts
+++ src/features/specs/utils/userInputToSpec/userInputToSpec.test.ts
@@ -1,14 +1,16 @@
 import { describe, expect, test } from "vitest";
 import { supportedUnits } from "@/db/schema/units";
+import { MOCK_INGREDIENTS } from "@/mocks/data/ingredients";
 import { userInputToSpec } from ".";
 
 describe("userInputToSpec", () => {
 	describe("parses unit", () => {
 		test.each(supportedUnits.options)('"%s"', (unit) => {
-			expect(userInputToSpec(`1 ${unit} gin`)).toEqual({
+			expect(userInputToSpec(`1 ${unit} gin`, MOCK_INGREDIENTS)).toEqual({
 				quantity: 1,
 				unit,
-				ingredient: "Gin",
+				ingredient: { name: "Gin" },
+				ingredientId: undefined,
 			});
 		});
 	});
@@ -17,10 +19,11 @@ describe("userInputToSpec", () => {
 		test.each(supportedUnits.options.map((unit) => unit.toUpperCase()))(
 			'"%s"',
 			(unit) => {
-				expect(userInputToSpec(`1 ${unit} gin`)).toEqual({
+				expect(userInputToSpec(`1 ${unit} gin`, MOCK_INGREDIENTS)).toEqual({
 					quantity: 1,
 					unit: unit.toLowerCase(),
-					ingredient: "Gin",
+					ingredient: { name: "Gin" },
+					ingredientId: undefined,
 				});
 			},
 		);
@@ -30,15 +33,33 @@ describe("userInputToSpec", () => {
 		test.each([
 			[
 				"5cl tanqueray Gin",
-				{ quantity: 5, unit: "cl", ingredient: "Tanqueray Gin" },
+				{
+					quantity: 5,
+					unit: "cl",
+					ingredient: { name: "Tanqueray Gin" },
+					ingredientId: undefined,
+				},
 			],
-			["2 _ml Suze", { quantity: 2, unit: "ml", ingredient: "Suze" }],
+			[
+				"2 _ml Suze",
+				{
+					quantity: 2,
+					unit: "ml",
+					ingredient: { name: "Suze" },
+					ingredientId: undefined,
+				},
+			],
 			[
 				"3   fl-oz   Lemon  juice ",
-				{ quantity: 3, unit: "fl_oz", ingredient: "Lemon juice" },
+				{
+					quantity: 3,
+					unit: "fl_oz",
+					ingredient: { name: "Lemon juice" },
+					ingredientId: undefined,
+				},
 			],
 		])('"%s"', (input, expected) => {
-			expect(userInputToSpec(input)).toEqual(expected);
+			expect(userInputToSpec(input, MOCK_INGREDIENTS)).toEqual(expected);
 		});
 	});
 });
