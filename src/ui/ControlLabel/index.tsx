11c3c15e4b6508013fc48a696b5144df74719f18
Nils Kaspersson <nils.kaspersson@live.se>
2025-06-19 22:39:43 +0200

    feat(ui): Take more props

--- src/ui/ControlLabel/index.tsx
+++ src/ui/ControlLabel/index.tsx
@@ -1,32 +1,44 @@
 import { clsx } from "clsx";
+import type { ComponentProps } from "react";
 import { Text } from "@/ui/Text";
 import styles from "./styles.module.css";
 
-export function ControlLabel(props: {
+type Props = {
 	className?: string;
 	label: React.ReactNode;
 	children: React.ReactNode;
 	required?: boolean;
 	htmlFor: string;
 	id: string;
-}) {
+} & ComponentProps<"label">;
+
+export function ControlLabel({
+	className,
+	label,
+	children,
+	required,
+	htmlFor,
+	id,
+	...props
+}: Props) {
 	return (
-		<div className={clsx(styles.base, props.className)}>
+		<div className={clsx(styles.base, className)}>
 			<Text
-				id={props.id}
+				{...props}
+				id={id}
 				as="label"
 				size={2}
 				weight={500}
 				compact
-				htmlFor={props.htmlFor}
+				htmlFor={htmlFor}
 				className={clsx(styles.label, {
-					required: props.required,
+					required,
 				})}
 			>
-				<span className={styles.text}>{props.label}</span>
+				<span className={styles.text}>{label}</span>
 			</Text>
 
-			{props.children}
+			{children}
 		</div>
 	);
 }
