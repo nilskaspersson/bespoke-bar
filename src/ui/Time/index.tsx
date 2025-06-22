3522ae1b3b197035611c0c98d46c0dee8f1ceef3
Nils Kaspersson <nils.kaspersson@live.se>
2025-06-22 14:09:28 +0200

    feat(ui): Use Text component

--- src/ui/Time/index.tsx
+++ src/ui/Time/index.tsx
@@ -2,8 +2,8 @@
 
 import { clsx } from "clsx";
 import { type ComponentProps, useEffect, useState } from "react";
+import { Text } from "@/ui/Text";
 import { formatRelativeTime } from "@/utils/dateTime";
-
 import styles from "./styles.module.css";
 
 export function Time({
@@ -14,7 +14,7 @@ export function Time({
 }: {
 	date: Date;
 	relativeThreshold?: number;
-} & Omit<ComponentProps<"time">, "children" | "dateTime">) {
+} & Omit<ComponentProps<typeof Text>, "children">) {
 	const [mounted, setMounted] = useState(false);
 
 	useEffect(() => setMounted(true), []);
@@ -32,13 +32,15 @@ export function Time({
 	const isoString = date.toISOString();
 
 	return (
-		<time
+		<Text
+			as="time"
 			dateTime={isoString}
 			title={isoString}
 			className={clsx(styles.time, className)}
+			compact
 			{...props}
 		>
 			{displayText}
-		</time>
+		</Text>
 	);
 }
