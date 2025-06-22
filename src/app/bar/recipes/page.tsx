10e31dfb6a06ef4be82b5f7cd4eb634a84027159
Nils Kaspersson <nils.kaspersson@live.se>
2025-06-22 14:07:50 +0200

    feat(recipes): Move Archive to separate page, implement RecipeTable

--- src/app/bar/recipes/page.tsx
+++ src/app/bar/recipes/page.tsx
@@ -1,48 +1,37 @@
 import type { Metadata } from "next";
-import Link from "next/link";
+import { readOrganisationMembers } from "@/features/organisation/actions/readOrganisationMembers";
 import { readBarRecipes } from "@/features/recipes/actions/readBarRecipes";
-import { RecipeName } from "@/features/recipes/components/RecipeName";
+import { RecipeTable } from "@/features/recipes/components/RecipeTable";
 import { LinkButton } from "@/ui/Button";
 import { Container } from "@/ui/Container";
+import { Flex } from "@/ui/Flex";
 import { Heading } from "@/ui/Heading";
 import { Icon } from "@/ui/Icon";
-import { recipeToUrlSlug } from "@/utils/url";
+import styles from "./page.module.css";
 
 export default async function RecipesPage() {
 	const recipes = await readBarRecipes();
-	const archivedRecipes = await readBarRecipes({ archivedRecipes: true });
+
+	const members = await readOrganisationMembers();
 
 	return (
-		<Container as="article">
-			<header>
+		<Container as="article" className={styles.container}>
+			<Flex as="header" gap={2} justifyContent="space-between">
 				<Heading level="h1">Recipes</Heading>
 
-				<LinkButton href="/bar/recipes/create" variant="solid" color="heavy">
-					Create Recipe
-					<Icon name="pen" />
-				</LinkButton>
-			</header>
+				<Flex gap={2}>
+					<LinkButton href="/bar/recipes/archive" variant="ghost">
+						Archive
+					</LinkButton>
 
-			<ul>
-				{recipes.map((recipe) => (
-					<li key={recipe.id}>
-						<Link href={`/bar/recipes/${recipe.id}/${recipeToUrlSlug(recipe)}`}>
-							<RecipeName recipe={recipe} />
-						</Link>
-					</li>
-				))}
-			</ul>
+					<LinkButton href="/bar/recipes/create" variant="solid" color="heavy">
+						Create Recipe
+						<Icon name="pen" />
+					</LinkButton>
+				</Flex>
+			</Flex>
 
-			<Heading level="h6">Archive</Heading>
-			<ul>
-				{archivedRecipes.map((recipe) => (
-					<li key={recipe.id}>
-						<Link href={`/bar/recipes/${recipe.id}/${recipeToUrlSlug(recipe)}`}>
-							<RecipeName recipe={recipe} />
-						</Link>
-					</li>
-				))}
-			</ul>
+			<RecipeTable recipes={recipes} members={members} />
 		</Container>
 	);
 }
