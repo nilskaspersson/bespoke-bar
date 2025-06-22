33af0e3aeee4dbce7276c6841edee220c0123a38
Nils Kaspersson <nils.kaspersson@live.se>
2025-06-22 09:48:13 +0200

    feat: Create dateTime and relativeTime formatters

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -13,3 +13,14 @@ export const volumeFormatter = new Intl.NumberFormat("en-GB", {
 	minimumFractionDigits: 0,
 	maximumFractionDigits: 1,
 });
+
+export const dateTimeFormatter = new Intl.DateTimeFormat("en-GB", {
+	year: "numeric",
+	month: "short",
+	day: "numeric",
+});
+
+export const relativeTimeFormatter = new Intl.RelativeTimeFormat("en-GB", {
+	numeric: "auto",
+	style: "long",
+});
