21ba63214b1a4d887609254c846da74a010eff2f
Nils Kaspersson <nils.kaspersson@live.se>
2025-07-06 18:05:55 +0200

    type(utils): Make sorting fns generic

--- src/utils/sorting.ts
+++ src/utils/sorting.ts
@@ -1,11 +1,10 @@
-import type { FilterFn, SortingFn } from "@tanstack/react-table";
+import type { Row } from "@tanstack/react-table";
 import { normalizeInput } from "@/utils";
 import { collator } from "@/utils/formatting";
 
-export const sortingFnCreatedAt: SortingFn<{
-	createdAt: Date;
-	name: string | null;
-}> = (rowA, rowB) => {
+export function sortingFnCreatedAt<
+	T extends { createdAt: Date; name: string | null },
+>(rowA: Row<T>, rowB: Row<T>) {
 	const dateA = rowA.original.createdAt.getTime();
 	const dateB = rowB.original.createdAt.getTime();
 
@@ -14,7 +13,14 @@ export const sortingFnCreatedAt: SortingFn<{
 	}
 
 	return collator.compare(rowA.original.name ?? "", rowB.original.name ?? "");
-};
+}
 
-export const globalFilterFn: FilterFn<unknown> = (row, columnId, filterValue) =>
-	normalizeInput(String(row.getValue(columnId) || "")).includes(filterValue);
+export function globalFilterFn<T>(
+	row: Row<T>,
+	columnId: string,
+	filterValue: string,
+) {
+	return normalizeInput(String(row.getValue(columnId) || "")).includes(
+		filterValue,
+	);
+}
