08e13210352be4adfb2168c2d540133fb7517ebe
Nils Kaspersson <nils.kaspersson@live.se>
2025-07-11 18:40:34 +0200

    chore: Clear component

--- src/features/recipes/components/PreviewDraftRecipe/index.tsx
+++ src/features/recipes/components/PreviewDraftRecipe/index.tsx
@@ -1,29 +1,9 @@
 "use client";
 
-import { useFormData } from "@conform-to/react/future";
-import { parseWithZod } from "@conform-to/zod/v4";
 import type { RefObject } from "react";
-import { type BaseRecipe, updateRecipeSchema } from "@/db/schema/recipes";
-import { RecipeInfo } from "@/features/recipes/components/RecipeInfo";
 
-export function PreviewDraftRecipe(props: {
+export function PreviewDraftRecipe(_props: {
 	formRef: RefObject<HTMLFormElement | null>;
 }) {
-	const draftRecipe: BaseRecipe | null = useFormData(
-		props.formRef,
-		(formData) => {
-			if (!formData) return null;
-			return parseWithZod(formData, { schema: updateRecipeSchema }).payload;
-		},
-	);
-
-	if (!draftRecipe) {
-		return null;
-	}
-
-	return (
-		<div>
-			<RecipeInfo recipe={draftRecipe} />
-		</div>
-	);
+	return <div></div>;
 }
