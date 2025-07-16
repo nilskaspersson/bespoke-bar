93bfed56d020092f40d030b20fe0f7667938d373
Nils Kaspersson <nils.kaspersson@live.se>
2025-07-16 11:12:51 +0200

    fix(utils): More generic handler

--- src/utils/handleKey.ts
+++ src/utils/handleKey.ts
@@ -1,13 +1,13 @@
 import type { KeyboardEvent } from "react";
 
-export function handleKey<T = unknown>(
+export function handleKey<T = Element>(
 	args: [
 		key: string,
-		fn: ((event: KeyboardEvent) => T) | undefined,
-		onCondition?: (event: KeyboardEvent) => boolean,
+		fn: ((event: KeyboardEvent<T>) => unknown) | undefined,
+		onCondition?: (event: KeyboardEvent<T>) => boolean,
 	][],
 ) {
-	return (event: KeyboardEvent) => {
+	return (event: KeyboardEvent<T>) => {
 		args.forEach(([key, fn, onCondition]) => {
 			if (
 				key === event.key &&
