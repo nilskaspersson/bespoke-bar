29f2843d517d3f58e78b6149f9fd8b6b49b7c208
Nils Kaspersson <nils.kaspersson@live.se>
2025-07-20 22:09:49 +0200

    fix(ui): Minor improvements to texture

--- src/ui/Noise/index.tsx
+++ src/ui/Noise/index.tsx
@@ -1,5 +1,13 @@
+import { clsx } from "clsx";
+import type { ComponentProps } from "react";
 import styles from "./styles.module.css";
 
-export function Noise() {
-	return <div className={styles.texture} role="presentation" />;
+export function Noise({ className, ...props }: ComponentProps<"div">) {
+	return (
+		<div
+			className={clsx(styles.texture, className)}
+			role="presentation"
+			{...props}
+		/>
+	);
 }
