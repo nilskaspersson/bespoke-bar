0067365fd9df0dee8d37271ced28b531424fd00f
Nils Kaspersson <nils.kaspersson@live.se>
2025-07-30 13:48:30 +0200

    feat: Add WakeLock toggle to footer (#52)

--- src/app/components/AppFooter/index.tsx
+++ src/app/components/AppFooter/index.tsx
@@ -2,6 +2,8 @@ import { clsx } from "clsx";
 import Link from "next/link";
 import type { ComponentProps } from "react";
 import { ThemePicker } from "@/app/components/ThemePicker";
+import { WakeLock } from "@/app/components/WakeLock";
+import { Flex } from "@/ui/Flex";
 import { Grid } from "@/ui/Grid";
 import { Heading } from "@/ui/Heading";
 import { Text } from "@/ui/Text";
@@ -18,61 +20,66 @@ export function AppFooter({
 			{...props}
 		>
 			<Grid className={styles.contain} gap={6}>
-				<nav className={styles.nav}>
-					<Grid gap={1}>
-						<Heading level="h6" size={2}>
-							Bar
-						</Heading>
+				<Flex gap={6} wrap justifyContent="space-between">
+					<nav className={styles.nav}>
+						<Grid gap={1}>
+							<Heading level="h6" size={2}>
+								Bar
+							</Heading>
 
-						<Text as="ul" size={2}>
-							<li>
-								<Link href="/bar" prefetch={false}>
-									Overview
-								</Link>
-							</li>
+							<Text as="ul" size={2}>
+								<li>
+									<Link href="/bar" prefetch={false}>
+										Overview
+									</Link>
+								</li>
 
-							<li>
-								<Link href="/bar/lists" prefetch={false}>
-									Lists
-								</Link>
-							</li>
+								<li>
+									<Link href="/bar/lists" prefetch={false}>
+										Lists
+									</Link>
+								</li>
 
-							<li>
-								<Link href="/bar/recipes" prefetch={false}>
-									Recipes
-								</Link>
-							</li>
+								<li>
+									<Link href="/bar/recipes" prefetch={false}>
+										Recipes
+									</Link>
+								</li>
 
-							<li>
-								<Link href="/bar/ingredients" prefetch={false}>
-									Ingredients
-								</Link>
-							</li>
-						</Text>
+								<li>
+									<Link href="/bar/ingredients" prefetch={false}>
+										Ingredients
+									</Link>
+								</li>
+							</Text>
+						</Grid>
+
+						<Grid gap={1}>
+							<Heading level="h6" size={2}>
+								Legal
+							</Heading>
+
+							<Text as="ul" size={2}>
+								<li>
+									<Link href="/terms" prefetch={false}>
+										Terms & conditions
+									</Link>
+								</li>
+
+								<li>
+									<Link href="/privacy" prefetch={false}>
+										Privacy policy
+									</Link>
+								</li>
+							</Text>
+						</Grid>
+					</nav>
+
+					<Grid gap={4} className={styles.settings}>
+						<ThemePicker />
+						<WakeLock size="small" />
 					</Grid>
-
-					<Grid gap={1}>
-						<Heading level="h6" size={2}>
-							Legal
-						</Heading>
-
-						<Text as="ul" size={2}>
-							<li>
-								<Link href="/terms" prefetch={false}>
-									Terms & conditions
-								</Link>
-							</li>
-
-							<li>
-								<Link href="/privacy" prefetch={false}>
-									Privacy policy
-								</Link>
-							</li>
-						</Text>
-					</Grid>
-				</nav>
-
-				<ThemePicker />
+				</Flex>
 
 				<div>
 					<Text as="div" size={1} light>
