981da0b45bb7e0541017def7c6af78f0dd9110c1
Nils Kaspersson <nils.kaspersson@live.se>
2025-12-21 20:25:35 +0100

    chore(gcp): Setup GCP

---
+++ src/gcp.ts
@@ -0,0 +1,11 @@
+export function getGCPClientCredentials() {
+	return process.env.GCP_PRIVATE_KEY
+		? {
+				credentials: {
+					client_email: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
+					private_key: process.env.GCP_PRIVATE_KEY,
+				},
+				projectId: process.env.GCP_PROJECT_ID,
+			}
+		: {}; // fallback to process.env.GOOGLE_APPLICATION_CREDENTIALS
+}
