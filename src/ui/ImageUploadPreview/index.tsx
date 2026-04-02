261f5b6a6e152082134adcd4aa09889bc32da0bd
Nils Kaspersson <nils.kaspersson@live.se>
2026-04-02 15:04:24 +0200

    chore(deps): Bump deps

--- src/ui/ImageUploadPreview/index.tsx
+++ src/ui/ImageUploadPreview/index.tsx
@@ -15,6 +15,7 @@ export function ImageUploadPreview({
 	return (
 		<div {...props} className={clsx(styles.container, className)}>
 			{src ? (
+				// biome-ignore lint/performance/noImgElement: This is a locally created image
 				<img src={src} className={styles.image} alt={alt} />
 			) : (
 				<Icon size={8} name="image" className={styles.icon} />
