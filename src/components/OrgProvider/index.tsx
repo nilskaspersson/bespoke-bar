cdea5ef727bd7b9b91c2aff2c5fea586a140da9a
Nils Kaspersson <nils.kaspersson@live.se>
2026-05-06 14:17:12 +0200

    feat(org): Handle zero-bar state

--- src/components/OrgProvider/index.tsx
+++ src/components/OrgProvider/index.tsx
@@ -1,16 +1,13 @@
 import { auth } from "@clerk/nextjs/server";
-import { forbidden } from "next/navigation";
+import { redirect } from "next/navigation";
 import { getOrCreateLocalOrganisation } from "@/features/organisation/api/getOrCreateLocalOrganisation";
 import { FormatterContextProvider } from "@/hooks/useFormatter";
 
 export async function OrgProvider({ children }: { children: React.ReactNode }) {
 	const { userId, orgId } = await auth.protect();
 
-	/**
-	 * Users should always have an organisation automatically created by Clerk.
-	 */
 	if (!orgId) {
-		forbidden();
+		redirect("/setup");
 	}
 
 	const organisation = await getOrCreateLocalOrganisation(orgId, userId);
